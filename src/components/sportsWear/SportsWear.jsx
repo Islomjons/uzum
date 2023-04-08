1ca94f49304a6c1bead833e288fce5242188d656
Islomjons <mukhammadalievislomjon@gmail.com>
2023-04-08 16:15:17 +0500

    chnged

---
+++ src/components/sportsWear/SportsWear.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import c from "./SportsWear.module.css"
+
+const SportsWear = () => {
+  return (
+    <div className={c.sportsWear}>SportsWear</div>
+  )
+}
+
+export default SportsWear
