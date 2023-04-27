e505a9d8c600a609a1af6844ac4e9895053c0131
Islomjons <mukhammadalievislomjon@gmail.com>
2023-04-27 22:22:20 +0500

    added new tag

--- src/components/phones/Phones.jsx
+++ src/components/phones/Phones.jsx
@@ -3,9 +3,14 @@ import c from "./Phones.module.css"
 
 const Phones = () => {
   return (
-    <div className={c.phones}>
-        <h1>Phones</h1>
-    </div>
+    <>
+        <div className={c.phones}>
+            <h1>Phones</h1>
+        </div>
+        <div className={c.phones__2}>
+            <h1>Phones 2</h1>
+        </div>
+    </>
   )
 }
 
