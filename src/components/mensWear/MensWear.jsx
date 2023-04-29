9aa573e1c448461ba960433ed1f46512cb52d135
Islomjons <mukhammadalievislomjon@gmail.com>
2023-04-29 23:36:17 +0500

    i added about me

--- src/components/mensWear/MensWear.jsx
+++ src/components/mensWear/MensWear.jsx
@@ -4,6 +4,11 @@ const MensWear = () => {
   return (
     <div>
         <h1>MensWear</h1>
+
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, asperiores?</p>
+            <h1>Islomjon Mukhammadaliev</h1>
+        </div>
     </div>
   )
 }
