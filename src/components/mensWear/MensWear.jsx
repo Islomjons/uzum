3eb6a2749b4d9c90dc71a7e2a47e43f7dc1eef47
Islomjons <mukhammadalievislomjon@gmail.com>
2023-05-05 22:26:17 +0500

    I added MensWear file

--- src/components/mensWear/MensWear.jsx
+++ src/components/mensWear/MensWear.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import c from "./MensWear.module.css"
 
 const MensWear = () => {
+  const [mensWearJson, setMensWearJson] = useState([])
+  useEffect(() => {
+      fetch("https://api.escuelajs.co/api/v1/products")
+          .then(response => response.json())
+          .then(data => console.log(data))
+  }, [])
   return (
-    <div>
-        <h1>MensWear</h1>
-    </div>
+    <div className={c.mensWear}>
+        <ul className={c.mensWear__list}>
+            
+        </ul>
+    </div>  
   )
 }
 
