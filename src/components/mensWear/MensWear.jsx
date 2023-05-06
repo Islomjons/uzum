32428e60ea6f9e1bbfe389526be77b80956a624d
Islomjons <mukhammadalievislomjon@gmail.com>
2023-05-06 22:43:46 +0500

    I added some of the files

--- src/components/mensWear/MensWear.jsx
+++ src/components/mensWear/MensWear.jsx
@@ -1,22 +1,53 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import c from "./MensWear.module.css"
+import { GrFormNext, GrFormPrevious } from "react-icons/gr"
+import { FiShoppingCart } from "react-icons/fi"
 
 const MensWear = () => {
+  const textDecorNone = {
+      textDecoration: "none"
+  }
   const [mensWearJson, setMensWearJson] = useState([])
   useEffect(() => {
       fetch("https://api.escuelajs.co/api/v1/products")
           .then(response => response.json())
-          .then(data => console.log(data))
+          .then(data => setMensWearJson(data.slice(85, 95)))
   }, [])
   return (
     <div className={c.mensWear}>
-        <ul className={c.mensWear__list}>
-            
-        </ul>
-        <div>
-          <p>salom</p>
-          <h1>Islomjon</h1>
+        <div className={c.mensWear__wrapper}>
+            <a className={c.mensWear__link} href="/">Erkaklar kiyimi</a>
+            <button className={c.mensWear__next__btn}>
+                <GrFormNext className={c.mensWear__next__icon}/>
+            </button>
         </div>
+        <button className={c.mensWear__carausel__btn}>
+            <GrFormPrevious className={c.mensWear__carausel__icon}/>
+        </button>
+        <ul className={c.mensWear__list}>
+            {
+                mensWearJson.map((mensWearss, id) => 
+                  <li className={c.mensWear__item} key={id}>
+                      <Link style={textDecorNone} to={`/pdp/${mensWearss.id}`}>
+                          <img className={c.mensWear__img} src={mensWearss.category.image} alt="" />
+                          <div className={c.mensWear__wrapper__info}>
+                              <p className={c.mensWear__title}>{mensWearss.title}</p>
+                              <p className={c.mensWear__price}>{mensWearss.price}</p>
+                          </div>
+                      </Link>
+                      <div className={c.mensWear__cart__btn__wrapper}>
+                          <button className={c.mensWear__cart__btn}>
+                              <FiShoppingCart className={c.mensWear__cart__icon}/>
+                          </button>
+                      </div>
+                  </li>
+                )
+            }
+        </ul>
+        <button className={c.mensWear__carausel__btn}>
+            <GrFormNext className={c.mensWear__carausel__icon}/>
+        </button>
     </div>  
   )
 }
