e69363ccf91f5ae1617fce32bdb28495601f77ab
Islomjons <mukhammadalievislomjon@gmail.com>
2023-05-26 02:16:37 +0500

    acsdv

--- src/components/forChildren/ForChildren.jsx
+++ src/components/forChildren/ForChildren.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import  c from "./ForChildren.module.css"
 
 const ForChildren = () => {
   return (
-    <div className={c.forChildren}>ForChildren</div>
+    <div className={c.forChildren}>
+        <div className={c.forChildren__container}>
+            <div className={c.forChildren__wrapper}>
+                  <Link className={c.forChildren__heading}>Bolalar uchun</Link>
+            </div>
+        </div>
+    </div>
   )
 }
 
